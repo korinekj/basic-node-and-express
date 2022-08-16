dedad781e0698c7a748c33410a7f0693b1d9d35d
Jarda Korinek <korinek14.j@gmail.com>
2022-08-16 20:24:53 +0200

    served json on specific route and used .env file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
+require("dotenv").config();
+
 let express = require("express");
 let app = express();
 
+//Serve an html file
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+//Serve JSON on a specific Route
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
+//Serve static assets (style.css ve složce public)
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
